954fe7f9c359243a6c14e54a73ae866d03bb39d6
Kathiravan K <kathirr007@gmail.com>
2023-12-18 23:31:27 +0530

    updated package.json and placeholder-blot.ts

--- src/placeholder-blot.ts
+++ src/placeholder-blot.ts
@@ -1,7 +1,7 @@
 import * as QuillTypes from 'quill'
 import {default as ParchmentTypes} from 'parchment'
 
-import type {Placeholder} from './placeholder'
+import {Placeholder} from './placeholder'
 
 export default function getPlaceholderBlot(Quill: QuillTypes.Quill): any {
   const Embed: typeof ParchmentTypes.Embed = Quill.import('blots/embed')
